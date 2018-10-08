64fd5f407f33a5c9e0fa599f3b9ef61bd26a99fb
Oleksandr Gurtovyi <gurtik7@gmail.com>
2018-10-08 18:01:55 +0300

    ^_^

--- src/main/java/RemoteNote/controller/LoginCommonController.java
+++ src/main/java/RemoteNote/controller/LoginCommonController.java
@@ -5,7 +5,6 @@ import RemoteNote.service.LoginCommonService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +18,7 @@ public class LoginCommonController {
     private LoginCommonService loginCommonService = new LoginCommonService();
 
     @ApiOperation(value = "Возвращает юзера из бд")
-    @RequestMapping(value = "/authorizeUser", method = RequestMethod.GET)
+    @RequestMapping(value = "/authorizeUser", method = RequestMethod.POST)
     @ResponseBody
     public User authorizeUser(
             @ApiParam(value = "Логин и пароль", required = true) @RequestBody JSONObject params) throws Exception {
